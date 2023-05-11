6895ce7ff8ba12548ba4ac041aa2f510e5bb5a4e
Loki99911 <nikita_kresik@i.ua>
2023-05-11 18:55:20 +0300

    add fn

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+import shipAriaSelect from "./shipAriaSelect.js"
 const ships = document.querySelectorAll(".ship");
 const sqweres = document.querySelectorAll(".yourSqwere");
 const dropField = document.querySelector(".yourField");
@@ -123,56 +124,56 @@ function drop(e) {
   counterElem.textContent--;
 }
 
-function shipAriaSelect(draggedShip, startSquere, shipLength, className, func) {
-  if (draggedShip.style.flexDirection === "row") {
-    const startFor = startSquere % 10 === 1 ? startSquere : startSquere - 1;
-    const endFor =
-      startSquere + shipLength > Math.ceil(startSquere / 10) * 10
-        ? Math.ceil(startSquere / 10) * 10
-        : startSquere + shipLength;
-    for (let i = startFor; i <= endFor; i++) {
-      func === "add"
-        ? i - 10 > 0 && document.getElementById(i - 10).classList.add(className)
-        : i - 10 > 0 &&
-          document.getElementById(i - 10).classList.remove(className);
-      func === "add"
-        ? i > 0 &&
-          i <= 100 &&
-          document.getElementById(i).classList.add(className)
-        : i > 0 &&
-          i <= 100 &&
-          document.getElementById(i).classList.remove(className);
-      func === "add"
-        ? i + 10 <= 100 &&
-          document.getElementById(i + 10).classList.add(className)
-        : i + 10 <= 100 &&
-          document.getElementById(i + 10).classList.remove(className);
-    }
-  } else {
-    const startFor = startSquere <= 10 ? startSquere : startSquere - 10;
-    const endFor =
-      startSquere + shipLength * 10 > 100
-        ? startSquere + Math.floor((100 - startSquere) / 10) * 10
-        : startSquere + shipLength * 10;
-    console.log(startFor, endFor);
-    for (let i = startFor; i <= endFor; i = i + 10) {
-      func === "add"
-        ? (i - 1) % 10 !== 0 &&
-          document.getElementById(i - 1).classList.add(className)
-        : (i - 1) % 10 !== 0 &&
-          document.getElementById(i - 1).classList.remove(className);
-      func === "add"
-        ? i > 0 &&
-          i <= 100 &&
-          document.getElementById(i).classList.add(className)
-        : i > 0 &&
-          i <= 100 &&
-          document.getElementById(i).classList.remove(className);
-      func === "add"
-        ? i % 10 !== 0 &&
-          document.getElementById(i + 1).classList.add(className)
-        : i % 10 !== 0 &&
-          document.getElementById(i + 1).classList.remove(className);
-    }
-  }
-}
+// function shipAriaSelect(draggedShip, startSquere, shipLength, className, func) {
+//   if (draggedShip.style.flexDirection === "row") {
+//     const startFor = startSquere % 10 === 1 ? startSquere : startSquere - 1;
+//     const endFor =
+//       startSquere + shipLength > Math.ceil(startSquere / 10) * 10
+//         ? Math.ceil(startSquere / 10) * 10
+//         : startSquere + shipLength;
+//     for (let i = startFor; i <= endFor; i++) {
+//       func === "add"
+//         ? i - 10 > 0 && document.getElementById(i - 10).classList.add(className)
+//         : i - 10 > 0 &&
+//           document.getElementById(i - 10).classList.remove(className);
+//       func === "add"
+//         ? i > 0 &&
+//           i <= 100 &&
+//           document.getElementById(i).classList.add(className)
+//         : i > 0 &&
+//           i <= 100 &&
+//           document.getElementById(i).classList.remove(className);
+//       func === "add"
+//         ? i + 10 <= 100 &&
+//           document.getElementById(i + 10).classList.add(className)
+//         : i + 10 <= 100 &&
+//           document.getElementById(i + 10).classList.remove(className);
+//     }
+//   } else {
+//     const startFor = startSquere <= 10 ? startSquere : startSquere - 10;
+//     const endFor =
+//       startSquere + shipLength * 10 > 100
+//         ? startSquere + Math.floor((100 - startSquere) / 10) * 10
+//         : startSquere + shipLength * 10;
+//     console.log(startFor, endFor);
+//     for (let i = startFor; i <= endFor; i = i + 10) {
+//       func === "add"
+//         ? (i - 1) % 10 !== 0 &&
+//           document.getElementById(i - 1).classList.add(className)
+//         : (i - 1) % 10 !== 0 &&
+//           document.getElementById(i - 1).classList.remove(className);
+//       func === "add"
+//         ? i > 0 &&
+//           i <= 100 &&
+//           document.getElementById(i).classList.add(className)
+//         : i > 0 &&
+//           i <= 100 &&
+//           document.getElementById(i).classList.remove(className);
+//       func === "add"
+//         ? i % 10 !== 0 &&
+//           document.getElementById(i + 1).classList.add(className)
+//         : i % 10 !== 0 &&
+//           document.getElementById(i + 1).classList.remove(className);
+//     }
+//   }
+// }
