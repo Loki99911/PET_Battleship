1ce28e4c8b6f943cbeeff79e4bc499b74ca39d3d
Loki99911 <nikita_kresik@i.ua>
2023-05-11 17:13:55 +0300

    Finishing logic add Ships

--- index.js
+++ index.js
@@ -42,70 +42,68 @@ function dragEnd() {
 }
 
 function dragOver(e) {
+  if (!draggedShip) return;
   e.preventDefault();
   const startSquere = +this.getAttribute("id");
-  if (draggedShip.style.flexDirection === "row") {
-    const startFor = startSquere % 10 === 1 ? startSquere : startSquere - 1;
-    const endFor =
-      startSquere + shipLength > Math.ceil(startSquere / 10) * 10
-        ? startSquere + shipLength - 1
-        : startSquere + shipLength;
-    console.log();
-    for (let i = startFor; i <= endFor; i++) {
-      i - 10 >= 0 && document.getElementById(i - 10).classList.add("over"); //"-"верхнее поле
-      i > 0 && i <= 100 && document.getElementById(i).classList.add("over");
-      i + 10 <= 100 && document.getElementById(i + 10).classList.add("over"); //"-"нижнее поле
-    }
-  } else {
-  }
-  // this.classList.add("over");
+  shipAriaSelect(draggedShip, startSquere, shipLength, "shipAria", "add");
 }
 
 function dragEnter(e) {
   if (!draggedShip) return;
   e.preventDefault();
-  // this.classList.add("over");
 }
 
 function dragLeave() {
   if (!draggedShip) return;
   const startSquere = +this.getAttribute("id");
-  if (draggedShip.style.flexDirection === "row") {
-    const startFor = startSquere % 10 === 1 ? startSquere : startSquere - 1;
-    const endFor =
-      startSquere + shipLength > Math.ceil(startSquere / 10) * 10 //7+4>    (2)
-        ? startSquere + shipLength - 1
-        : startSquere + shipLength;
-    for (let i = startFor; i <= endFor; i++) {
-      i - 10 >= 0 && document.getElementById(i - 10).classList.remove("over"); //"-"верхнее поле
-      i > 0 && i <= 100 && document.getElementById(i).classList.remove("over");
-      i + 10 <= 100 && document.getElementById(i + 10).classList.remove("over"); //"-"нижнее поле
-    }
-  } else {
-  }
+  shipAriaSelect(draggedShip, startSquere, shipLength, "shipAria", "remove");
   console.log(this);
-
-  // this.classList.remove("over");
 }
 
 function drop(e) {
-  const activeSq = this.getAttribute("id");
+  const activeSq = +this.getAttribute("id");
   if (!draggedShip) return;
   let newThis = this;
 
   if (
-    +activeSq + shipLength - 1 > Math.ceil(+activeSq / 10) * 10 &&
+    activeSq + shipLength - 1 > Math.ceil(activeSq / 10) * 10 &&
     draggedShip.style.flexDirection === "row"
   ) {
-    return this.classList.remove("over");
+    return shipAriaSelect(
+      draggedShip,
+      activeSq,
+      shipLength,
+      "shipAria",
+      "remove"
+    );
   }
 
   if (
-    +activeSq + (shipLength - 1) * 10 > 100 &&
+    activeSq + (shipLength - 1) * 10 > 100 &&
     draggedShip.style.flexDirection === "column"
   ) {
-    return this.classList.remove("over");
+    return shipAriaSelect(
+      draggedShip,
+      activeSq,
+      shipLength,
+      "shipAria",
+      "remove"
+    );
   }
+
+  if (
+    dropField.querySelector(".shipPart_1.shipAria") ||
+    dropField.querySelector(".shipPart_2.shipAria") ||
+    dropField.querySelector(".shipPart_3.shipAria") ||
+    dropField.querySelector(".shipPart_4.shipAria")
+  )
+    return shipAriaSelect(
+      draggedShip,
+      activeSq,
+      shipLength,
+      "shipAria",
+      "remove"
+    );
 
   newThis.classList.add("shipPart_1");
   for (let i = 2; i <= shipLength; i++) {
@@ -113,49 +111,68 @@ function drop(e) {
       newThis.nextElementSibling.classList.add(`shipPart_${i}`);
       newThis = newThis.nextElementSibling;
     } else {
-      const nextElement = document.getElementById(+activeSq + 10 * (i - 1));
+      const nextElement = document.getElementById(activeSq + 10 * (i - 1));
       nextElement.classList.add(`shipPart_${i}`);
     }
   }
-  this.classList.remove("over");
-  // console.log(tempBusySq);
+
+  // shipAriaSelect(draggedShip, activeSq, shipLength, "over", "add");
+  shipAriaSelect(draggedShip, activeSq, shipLength, "shipAria", "remove");
   const counterElem =
     draggedShip.parentNode.nextElementSibling.querySelector(".ships_counter");
   counterElem.textContent--;
 }
 
-// if (draggedShip.style.flexDirection === "row") {
-//   if (draggedShip.classList.contains("x1")) {
-//     this.classList.add("shipPart_1");
-//   } else if (draggedShip.classList.contains("x2")) {
-//     this.classList.add("shipPart_1");
-//     this.nextElementSibling.classList.add("shipPart_2");
-//   } else if (draggedShip.classList.contains("x3")) {
-//     this.classList.add("shipPart_1");
-//     this.nextElementSibling.classList.add("shipPart_2");
-//     this.nextElementSibling.nextElementSibling.classList.add("shipPart_3");
-//   } else {
-//     this.classList.add("shipPart_1");
-//     this.nextElementSibling.classList.add("shipPart_2");
-//     this.nextElementSibling.nextElementSibling.classList.add("shipPart_3");
-//     this.nextElementSibling.nextElementSibling.nextElementSibling.classList.add(
-//       "shipPart_4"
-//     );
-//   }
-// } else {
-//   if (draggedShip.classList.contains("x1")) {
-//     this.classList.add("shipPart_1");
-//   } else if (draggedShip.classList.contains("x2")) {
-//     this.classList.add("shipPart_1");
-//     document.getElementById(+activeSq + 10).classList.add("shipPart_2");
-//   } else if (draggedShip.classList.contains("x3")) {
-//     this.classList.add("shipPart_1");
-//     document.getElementById(+activeSq + 10).classList.add("shipPart_2");
-//     document.getElementById(+activeSq + 20).classList.add("shipPart_3");
-//   } else {
-//     this.classList.add("shipPart_1");
-//     document.getElementById(+activeSq + 10).classList.add("shipPart_2");
-//     document.getElementById(+activeSq + 20).classList.add("shipPart_3");
-//     document.getElementById(+activeSq + 30).classList.add("shipPart_4");
-//   }
-// }
+function shipAriaSelect(draggedShip, startSquere, shipLength, className, func) {
+  if (draggedShip.style.flexDirection === "row") {
+    const startFor = startSquere % 10 === 1 ? startSquere : startSquere - 1;
+    const endFor =
+      startSquere + shipLength > Math.ceil(startSquere / 10) * 10
+        ? Math.ceil(startSquere / 10) * 10
+        : startSquere + shipLength;
+    for (let i = startFor; i <= endFor; i++) {
+      func === "add"
+        ? i - 10 > 0 && document.getElementById(i - 10).classList.add(className)
+        : i - 10 > 0 &&
+          document.getElementById(i - 10).classList.remove(className);
+      func === "add"
+        ? i > 0 &&
+          i <= 100 &&
+          document.getElementById(i).classList.add(className)
+        : i > 0 &&
+          i <= 100 &&
+          document.getElementById(i).classList.remove(className);
+      func === "add"
+        ? i + 10 <= 100 &&
+          document.getElementById(i + 10).classList.add(className)
+        : i + 10 <= 100 &&
+          document.getElementById(i + 10).classList.remove(className);
+    }
+  } else {
+    const startFor = startSquere <= 10 ? startSquere : startSquere - 10;
+    const endFor =
+      startSquere + shipLength * 10 > 100
+        ? startSquere + Math.floor((100 - startSquere) / 10) * 10
+        : startSquere + shipLength * 10;
+    console.log(startFor, endFor);
+    for (let i = startFor; i <= endFor; i = i + 10) {
+      func === "add"
+        ? (i - 1) % 10 !== 0 &&
+          document.getElementById(i - 1).classList.add(className)
+        : (i - 1) % 10 !== 0 &&
+          document.getElementById(i - 1).classList.remove(className);
+      func === "add"
+        ? i > 0 &&
+          i <= 100 &&
+          document.getElementById(i).classList.add(className)
+        : i > 0 &&
+          i <= 100 &&
+          document.getElementById(i).classList.remove(className);
+      func === "add"
+        ? i % 10 !== 0 &&
+          document.getElementById(i + 1).classList.add(className)
+        : i % 10 !== 0 &&
+          document.getElementById(i + 1).classList.remove(className);
+    }
+  }
+}
