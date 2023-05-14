85062923afbe0d809679a806365c51e4172b5cdc
Loki99911 <nikita_kresik@i.ua>
2023-05-14 10:28:27 +0300

    enemy fild

--- js/startGame.js
+++ js/startGame.js
@@ -1,19 +1,30 @@
+import shipAriaSelect from "./shipAriaSelect.js";
 import shipPut from "./shipPut.js";
 
 const startGame = () => {
   const dropField = document.querySelector(".enemyField");
-  const ships = [4, 3, 3, 2, 2, 2, 1, 1, 1, 1];
+  const ships = [4, 3, 3];
 
-  const putEnemyShip = (length) => {
-    const shipDirection = Math.round(Math.random()) === 0 ? "row" : "column";
-    const shipId = Math.round(Math.random() * 100);
-    const activeElem = document.getElementById(shipId);
-    const shipOnDesk = shipPut(length, shipDirection, activeElem, dropField);
-    console.log(shipOnDesk);
-  };
+  // const putEnemyShip = (length) => {
+  //   const shipDirection = Math.round(Math.random()) === 0 ? "row" : "column";
+  //   const shipId = Math.round(Math.random() * 100);
+  //   const activeElem = document.getElementById(shipId);
+  //   const shipOnDesk = shipPut(length, shipDirection, activeElem, dropField);
+  //   console.log(shipOnDesk);
+  // };
 
   ships.map((shipLength) => {
-    putEnemyShip(shipLength);
+    let shipOnDesk = false;
+    while (!shipOnDesk) {
+      const shipDirection = Math.round(Math.random()) === 0 ? "row" : "column";
+      const shipId = Math.round(Math.random() * 100);
+      const activeElem = document.getElementById(shipId);
+      shipAriaSelect(shipDirection, shipId, shipLength, "shipAria", "add");
+      shipOnDesk = shipPut(shipLength, shipDirection, activeElem, dropField);
+      console.log(shipOnDesk);
+    }
+    //   shipAriaSelect(shipDirection, shipId, length, "shipAria", "add");
+    // putEnemyShip(shipLength);
   });
 };
 export default startGame;
